06cfec5e4ccfb2ff1a81c03170bab1f39f0169d2
Facundo-Mazziotti <mazziottifacu@gmail.com>
2026-06-16 11:16:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,36 +24,96 @@ DARIO       |       50%
 
 
 
-cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
-let precioBase: any
+function calcularTotal(id: string, cantidad: string, codigoDescuento: string): number {
   let cantidadNum: number = Number(cantidad);
-  function calcularTotal(id: string, cantidad: number, codigoDescuento: string): number{
-  if (id==="sabrina"){
-   precioBase=1000
+  let TOTAL = 0;
+  if (id === 'sabrina') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 1000 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 1000 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 1000 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 1000;
+    }
+  } else if (id === 'kgatlw') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 700 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 700 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 700 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 700;
+    }
+  } else if (id === 'lali') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 500 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 500 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 500 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 500;
+    }
+  } else if (id === 'magdalena') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 600 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 600 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 600 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 600;
+    }
+  } else if (id === 'viagra') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 400 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 400 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 400 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 400;
+    }
+  } else if (id === 'dillom') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 350 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 350 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 350 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 350;
+    }
+  } else if (id === 'marilina') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 200 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 200 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 200 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 200;
+    }
+  } else if (id === 'mugre') {
+    if (codigoDescuento === 'TIC10') {
+      TOTAL = cantidadNum * 150 * 0.9;
+    } else if (codigoDescuento === 'TIC20') {
+      TOTAL = cantidadNum * 150 * 0.8;
+    } else if (codigoDescuento === 'DARIO') {
+      TOTAL = cantidadNum * 150 * 0.5;
+    } else {
+      TOTAL = cantidadNum * 150;
+    }
   }
-  else if (id==="kgatlw"){
-     precioBase=700
-  }
-  else if (id==="lali"){
-    precioBase=500}
-  else if (id==="magdalena"){
-    precioBase=600}
-    else if (id==="viagra")
-    { precioBase=400}
-    else if(id==="dillom")
-    {precioBase=350}
-    else if(id==="marilina"){
-      precioBase=200}
-      else if(id==="mugre"){
-        precioBase=150
-      }
+  return TOTAL;
+}
 
-    if (codigoDescuento==="TIC10"){return precioBase*cantidad*0.9}
-    else if (codigoDescuento==="TIC20"){return precioBase*cantidad*0.8}
-    else if(codigoDescuento==="DARIO"){return precioBase*cantidad*0.5}
-    else {return precio*cantidad}
-  }
-  let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
+cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
+  let cantidadNum: number = Number(cantidad);
+  let precio: number = calcularTotal(id, cantidad, codigoDescuento);
   enviarAlFrontend('mostrarPrecio', precio);
 });
 
